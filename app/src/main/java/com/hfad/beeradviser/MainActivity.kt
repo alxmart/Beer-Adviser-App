2b393e6ab3d915b81c739e3b986ee552d1e84972
alxmart <alxmartinez@gmail.com>
2023-01-06 14:51:00 -0300

    Created setOnCLickListener

--- app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hfad.beeradviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+
+        findBeer.setOnClickListener {
+            //code
+        }
     }
 }
